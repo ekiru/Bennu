f2afa72e73b2fe4471e26644f70dde16e9437847
Tyler Leslie Curtis <tyler.l.curtis@gmail.com>
2010-07-05 03:41:04 -0500

    Switch to 'grammar;' style for SIC::Grammar, clean up the grammar, and require that it parses the whole string.

--- lib/SIC/Grammar.pm
+++ lib/SIC/Grammar.pm
@@ -1,42 +1,46 @@
-grammar SIC::Grammar {
-    token TOP { ^
-        <version_line>
-        <.newline> ** 2
-        <environment>
-        <.newline>
-        <block> ** [<.newline> <.newline>]
-    }
+grammar SIC::Grammar;
 
-    token newline { "\n" }
-
-    token indent { "    " }
-
-    token version_line {
-        'This is SIC v' <version>
-        || <.panic: 'Invalid version line.'>
-    }
-
-    token version { (\d\d\d\d '.' \d\d) }
-    
-    token environment {
-        'environment:' <.newline>
-            [<indent> <pad> <.newline>]+
-             <indent> 'containers:' \s+ '[]' <.newline>
-    }
-
-    token pad { <ident> ':' }
-
-    token block {
-        'block' \s+ "'" <ident> "':" <.newline>
-            [\s+ <statement> ] ** [<.newline>]
-    }
-
-    proto token statement {...}
-
-    token statement:sym<=> { <variable> <.ws> <sym> <.ws> <constant> }
-    token statement:sym<say> { <sym> <.ws> <variable> }
-
-    token variable { "\$" (\d+) }
-
-    token constant { (\d+) }
+token TOP { 
+    ^
+    <version_line>
+    <.newline> ** 2
+    <environment>
+    <.newline>
+    <block> ** [<.newline> <.newline>]
+    <.ws> $
 }
+
+token newline { "\n" }
+
+token indent { "    " }
+
+token version_line {
+    'This is SIC v' <version>
+    || <.panic: 'Invalid version line.'>
+}
+
+token version { (\d\d\d\d '.' \d\d) }
+    
+token environment {
+    'environment:' <.newline>
+      [<indent> <pad>]+
+      <indent> 'containers:' <.ws> "[]" <.newline>
+}
+
+token pad {
+    <ident> ':' <.newline>
+}
+
+token block {
+    'block' <.ws> "'" <ident> "':" <.newline>
+      [<.ws> <statement> ] ** [<.newline>]
+}
+
+proto token statement {...}
+
+token statement:sym<=> { <variable> <.ws> <sym> <.ws> <constant> }
+token statement:sym<say> { <sym> <.ws> <variable> }
+
+token variable { "\$" (\d+) }
+
+token constant { (\d+) }
