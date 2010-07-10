0e3c3320bf9ba6a0c2137f07e7d858a8839a2239
Tyler Leslie Curtis <tyler.l.curtis@gmail.com>
2010-07-10 09:56:36 -0500

    int_vtable -> bennu_int_vtable, initialize it.

--- runtime/int.c
+++ runtime/int.c
@@ -1,6 +1,6 @@
 #include "int.h"
 
-bennu_vtable *int_vtable;
+bennu_vtable *bennu_int_vtable;
 
 struct bennu_int {
 	bennu_vtable *_vt[0];
@@ -8,5 +8,6 @@ struct bennu_int {
 };
 
 void bennu_int_init(void) {
-
+	bennu_int_vtable = (bennu_vtable *)BENNU_OBJ_SEND(bennu_object_vtable,
+	                                                  bennu_s_delegated);
 }
