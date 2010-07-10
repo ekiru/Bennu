a4c5a61ae3ffa80dee0875a486e1d82adbfeac71
Tyler Leslie Curtis <tyler.l.curtis@gmail.com>
2010-07-10 00:16:19 -0500

    Add empty bennu_int struct.

--- runtime/int.c
+++ runtime/int.c
@@ -1,5 +1,9 @@
 #include "int.h"
 
+struct bennu_int {
+  bennu_vtable *_vt[0];
+};
+
 void bennu_int_init(void) {
 
 }
--- runtime/int.h
+++ runtime/int.h
@@ -1,6 +1,9 @@
 #ifndef BENNU_INT_H
 #define BENNU_INT_H
 
+
+typedef struct bennu_int bennu_int;
+
 void bennu_int_init(void);
 
 #endif /* BENNU_INT_H */
