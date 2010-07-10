6b15898d099c8783a048d16711e17e2c3f9a62b7
Tyler Leslie Curtis <tyler.l.curtis@gmail.com>
2010-07-10 13:29:21 -0500

    Add bennu_say function to call say message from C more conveniently.

--- runtime/setting.c
+++ runtime/setting.c
@@ -5,3 +5,7 @@ bennu_object *bennu_s_say;
 void bennu_setting_init(void) {
 	bennu_s_say = bennu_symbol_intern(0, 0, "say");
 }
+
+bennu_object *bennu_say(bennu_object *self) {
+	BENNU_OBJ_SEND(self, bennu_s_say);
+}
--- runtime/setting.h
+++ runtime/setting.h
@@ -7,4 +7,6 @@ extern bennu_object *bennu_s_say;
 
 void bennu_setting_init(void);
 
+bennu_object *bennu_say(bennu_object *self);
+
 #endif /* BENNU_SETTING_H */
