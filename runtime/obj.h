ef7fb5158b3599985fd74aed92c7c309cc271465
Tyler Leslie Curtis <tyler.l.curtis@gmail.com>
2010-07-09 23:16:22 -0500

    Pull ICACHE and MCACHE into header files.

---
+++ runtime/obj.h
@@ -0,0 +1,7 @@
+#ifndef BENNU_OBJ_H
+#define BENNU_OBJ_H
+
+#define BENNU_OBJ_ICACHE 1	/* nonzero to enable point-of-send inline cache */
+#define BENNU_OBJ_MCACHE 1	/* nonzero to enable global method cache        */
+
+#endif /* BENNU_OBJ_H */
