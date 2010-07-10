643665b8ff74d7ae06015a78dfaddc382059c7e9
Tyler Leslie Curtis <tyler.l.curtis@gmail.com>
2010-07-10 00:03:02 -0500

    Add bennu.c, bennu.h, and bennu_init function.

---
+++ runtime/bennu.c
@@ -0,0 +1,6 @@
+#include "bennu.h"
+#include "obj.h"
+
+void bennu_init(void) {
+	bennu_obj_init();
+}
---
+++ runtime/bennu.h
@@ -0,0 +1,6 @@
+#ifndef BENNU_BENNU_H
+#define BENNU_BENNU_H
+
+void bennu_init(void);
+
+#endif /* BENNU_BENNU_H */
