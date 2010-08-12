a248d3f7b1710ca9d2ad4138af20d78073deef49
tcurtis <tyler.l.curtis@gmail.com>
2010-08-12 01:04:39 -0500

    Switch existing bootstrap to use get/set-attribute.

--- lib/Bennu/Metamodel.pm
+++ lib/Bennu/Metamodel.pm
@@ -17,10 +17,14 @@ class Bennu::Attribute { ... }
 class Bennu::Method { ... }
 
 class Bennu::Mu is rw {
-    has Bennu::REPR $._REPR;
+    has Bennu::REPR $._REPR .= new;
     # Fake attributes are stored in the _REPR.
     # has Bennu::ClassHOW $._HOW;
     # has Bennu::ClassWHAT $._WHAT;
+}
+
+class Bennu::REPR {
+    has %.storage;
 }
 
 class Bennu::ClassHOW is Bennu::Mu {
@@ -29,10 +33,6 @@ class Bennu::ClassHOW is Bennu::Mu {
     # has Bennu::Attribute @.attributes;
     # has Bennu::ClassHOW @.parents;
     # has Bennu::ClassREPR $.REPR;
-}
-
-class Bennu::REPR is Bennu::Mu {
-
 }
 
 class Bennu::ClassWHAT is Bennu::Mu {
@@ -66,20 +66,28 @@ our Bennu::ClassHOW $HOWClassWHAT;
 our Bennu::ClassHOW $HOWAttribute;
 our Bennu::ClassHOW $HOWMethod;
 
+our sub get-attribute (Bennu::Mu $self, Str $attr) {
+    $self._REPR.storage{$attr};
+}
+
+our sub set-attribute (Bennu::Mu $self, Str $attr, $value) {
+    $self._REPR.storage{$attr} = $value;
+}
+
 our Bennu::ClassHOW sub ClassHOW-create (Str $name) {
     my Bennu::ClassHOW $classHOW .= new;
-    $classHOW._REPR<name> = $name;
-    $classHOW._REPR<_HOW> = $HOWClassHOW;
+    set-attribute $classHOW, 'ClassHOW::$!name', $name;
+    set-attribute $classHOW, 'Mu::!HOW', $HOWClassHOW;
     $classHOW;
 }
 
 our sub ClassHOW-add-parent (Bennu::ClassHOW $self, Bennu::ClassHOW $parent) {
-    $self._REPR<parents>.push($parent);
+    get-attribute($self, 'ClassHOW::@!parents').push($parent);
 }
 
 our sub metamodel-init () {
     $HOWClassHOW = ClassHOW-create('ClassHOW');
-    $HOWClassHOW._REPR<_HOW> = $HOWClassHOW;
+    set-attribute $HOWClassHOW, 'Mu::$!HOW', $HOWClassHOW;
 
     $HOWMu = ClassHOW-create('Mu');
     classHOW-add-parent($HOWClassHOW, $HOWMu);
