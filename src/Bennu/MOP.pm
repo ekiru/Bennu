7ade1b23720933b051a35f786d5e27e5a67722f5
tcurtis <tyler.l.curtis@gmail.com>
2010-10-05 13:46:17 -0500

    [MOP] Add ClassHOW.new-type-object method.

--- src/Bennu/MOP.pm
+++ src/Bennu/MOP.pm
@@ -2,8 +2,15 @@ use MooseX::Declare;
 
 class Bennu::MOP::Mu {
     has how => (is => 'rw');
+    has defined => (is => 'ro');
 }
 
 class Bennu::MOP::ClassHOW extends Bennu::MOP::Mu {
+    has name => (is => 'rw');
+
+    method new_type_object() {
+        Bennu::MOP::Mu->new(defined => 0, how => $self);
+    }
+
     method add_attribute($obj, $attribute) {...}
 }
--- src/Bennu/MOP.pm6
+++ src/Bennu/MOP.pm6
@@ -2,8 +2,15 @@ module Bennu::MOP;
 
 class Mu {
     has $.how is rw;
+    has $.defined;
 }
 
 class ClassHOW is Mu {
+    has $.name is rw;
+
+    method new-type-object {
+        Mu.new(:defined(False), :how(self));
+    }
+
     method add-attribute($obj, $attribute) {...}
 }
